a74420f217035cad6ad11915598662475f7d36b6
LZanardii <leonardo.szanardi@gmail.com>
2024-08-15 18:49:04 -0300

    feat: bootstrap

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
 import Example from "src/components/Example"
-
+import { Button } from 'react-bootstrap';
 const Home = () => {
   return (
     <>
       <Example />
+      <Button>Bootstrap</Button>
     </>
   )
 }
